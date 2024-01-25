5bd7b1059f3f2e401d2992b82b5eaa082cac81a8
alexa-whitney <alexawhitney@gmail.com>
2024-01-25 16:07:48 -0700

    Added weather icons

--- src/DisplayWeather.js
+++ src/DisplayWeather.js
@@ -11,17 +11,27 @@ function DisplayWeather({ weatherData }) {
 		return <p>Error: {weatherData.message}</p>
 	}
 
+	// Base URL for weather icons from openweathermap.org structure of 
+	// https://openweathermap.org/img/wn/10d@2x.png:
+	const iconBaseUrl = "http://openweathermap.org/img/wn/";
+
 	// Otherwise ('cod' == 200), display the weather forecast.
 	return (
 		<div className="weather-data-card">
 			<h2>Weather in {weatherData.name}</h2>
 			<p>Temperature: {weatherData.main.temp}°</p>
 			<p>Feels Like: {weatherData.main.feels_like}°</p>
-      <p>Weather: {weatherData.weather[0].main} - {weatherData.weather[0].description}</p>
-      <p>Humidity: {weatherData.main.humidity}%</p>
-      <p>Pressure: {weatherData.main.pressure} hPa</p>
-      <p>Wind Speed: {weatherData.wind.speed} m/s</p>
-      <p>Cloudiness: {weatherData.clouds.all}%</p>
+			<p>Weather: {weatherData.weather[0].main} - {weatherData.weather[0].description}</p>
+
+			{/* Displaying the weather icon */}
+			<img
+				src={`${iconBaseUrl}${weatherData.weather[0].icon}@2x.png`}
+				alt={weatherData.weather[0].description}
+			/>
+			<p>Humidity: {weatherData.main.humidity}%</p>
+			<p>Pressure: {weatherData.main.pressure} hPa</p>
+			<p>Wind Speed: {weatherData.wind.speed} m/s</p>
+			<p>Cloudiness: {weatherData.clouds.all}%</p>
 		</div>
 	);
 }
